97ea49a1fd4800ef9b46c2ce0b5eb69fdc6436b5
hshim <ggrteas@gmail.com>
2025-06-15 13:20:14 +0900

    init

---
+++ src/main/kotlin/com/hshim/escapemanager/EscapemanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.hshim.escapemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EscapemanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EscapemanagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/hshim/escapemanager/EscapemanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hshim.escapemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EscapemanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
