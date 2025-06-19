6f4c2c02b8045020d44f4e2c7f03450dbd356db6
심혁 <shimhyuck@simhyeog-ui-MacBookPro.local>
2025-06-20 08:56:28 +0900

    test exception 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation(project(":kotlin-utils"))
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
